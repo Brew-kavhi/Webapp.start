8a5c803d46440a3808c6465a4e6ab9859d15935e
Marius Goehring <marius.goehring@helsinki-systems.de>
2024-10-14 21:35:27 +0200

    style(format): format a few files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,14 @@
 // Register the service-worker
-window.addEventListener('load', () =>{
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-        console.log('Service Worker registration succesful with scope:', registration.scope); 
-    }, err => {
-        console.log('Service-Worker registration failed: ', err);
-    });
+window.addEventListener('load', () => {
+	navigator.serviceWorker.register('/service-worker.js').then(
+		(registration) => {
+			console.log(
+				'Service Worker registration succesful with scope:',
+				registration.scope
+			);
+		},
+		(err) => {
+			console.log('Service-Worker registration failed: ', err);
+		}
+	);
 });
