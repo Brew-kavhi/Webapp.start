ed6a7d8b190a3214b5bd69de63816a38c1ea87ef
Marius Goehring <marius.goehring@helsinki-systems.de>
2024-10-30 21:25:11 +0100

    feat(frontend): reset password
    Include components for resetting password:  one for query a token, and
    one for setting the new password

---
+++ frontend/components/login/PasswordResetToken.ts
@@ -0,0 +1,86 @@
+/*t as a card.
+ * This code is part of the Brew.Kahvi frontend.
+ *
+ * Author: Marius Goehring
+ * Website: mariusgoehring.de
+ * E-Mail: kavhi@mariusgoehring.de
+ *
+ * Version: 0.0.1
+ * License: MIT
+ * Copyright Marius Goehring
+ */
+import { TemplatedComponent } from '/components/utils/TemplatedComponent.ts';
+import { authenticateUserBiometricCredentials } from '/js/pwa.ts';
+import { USER_API_HOST } from '/js/const/host.ts';
+import templateHTML from '/components/login/password_reset_token.html';
+import { FieldScheme, InputType } from '../../js/types/FieldScheme';
+
+export class PasswordResetToken extends TemplatedComponent {
+	private passwordInput: HTMLElement;
+	private passwordField: FieldScheme;
+
+	constructor() {
+		super();
+		this.passwordField = {
+			name: 'password',
+			label: 'pasword',
+			type: InputType.PassworConfirmation,
+			required: true
+		};
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = this.dynamicHTML(templateHTML);
+		// replace slot-anem ith the password input
+		this.passwordInput = document.createElement('input-field');
+		this.passwordInput.field = this.passwordField;
+		var slot = this.shadowRoot?.querySelector(
+			`div[slot-name="password"]`
+		);
+		slot?.appendChild(this.passwordInput);
+		this.addBootstrap();
+		this.shadowRoot.addEventListener('submit', this);
+	}
+
+	async handleEvent(e) {
+		if (e.target.id == 'password_form' && e.type == 'submit') {
+			e.preventDefault();
+			this.checkToken();
+		}
+	}
+
+	checkToken() {
+		if (this.passwordInput.validate()) {
+			const password = this.passwordInput.value;
+			console.log(password);
+			const queryString = window.location.search;
+			const urlParams = new URLSearchParams(queryString);
+			const email = urlParams.get("email");
+			if (!email) {
+				return;
+			}
+			fetch(`${USER_API_HOST}/user/validatepasswordtoken${queryString}`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username: email, password: password }),
+			}).then((response) =>{
+				if (response.status == 200) {
+					console.log(response);
+				} else if(response.status == 404) {
+					// email is worong
+					alert("something went wrong, check if you are on the right crednetial");
+				} else if(response.status == 401) {
+					// invalid token
+					alert("the token is not valid");
+				}
+			}).catch((error) =>{});
+		}
+	}
+}
+customElements.define('reset-token', PasswordResetToken);
---
+++ frontend/components/login/ResetPassword.ts
@@ -0,0 +1,59 @@
+/*t as a card.
+ * This code is part of the Brew.Kahvi frontend.
+ *
+ * Author: Marius Goehring
+ * Website: mariusgoehring.de
+ * E-Mail: kavhi@mariusgoehring.de
+ *
+ * Version: 0.0.1
+ * License: MIT
+ * Copyright Marius Goehring
+ */
+import { TemplatedComponent } from '/components/utils/TemplatedComponent.ts';
+import { authenticateUserBiometricCredentials } from '/js/pwa.ts';
+import { USER_API_HOST } from '/js/const/host.ts';
+import templateHTML from '/components/login/reset_password.html';
+
+export class ResetPassword extends TemplatedComponent {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	render() {
+		this.shadowRoot.innerHTML = this.dynamicHTML(templateHTML);
+		this.addBootstrap();
+		this.shadowRoot.addEventListener('submit', this);
+	}
+
+	async handleEvent(e) {
+		if (e.target.id == 'reset-form' && e.type == 'submit') {
+			e.preventDefault();
+			const username = this.shadowRoot.getElementById('username').value;
+
+			// Send registration request
+			const response = await fetch(`${USER_API_HOST}/user/reset_password`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username }),
+			});
+			console.log(response);
+
+			if (response.ok) {
+				window.location.href = '/';
+			} else {
+				if (response.status == 404) {
+					this.shadowRoot.querySelector('#error-message').innerHTML="Error";
+				} else {
+				alert('Login failed');
+				}
+			}
+		}
+	}
+}
+customElements.define('reset-password', ResetPassword);
